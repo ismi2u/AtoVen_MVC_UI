43b95945740ed651bf9e759772818980ce47cc84
HabeebAtominos <habeebullah@atominosconsulting.com>
2022-02-10 16:58:29 +0530

    Changed in Login redirection and password reset and company post

---
+++ AtoVen_MVC_UI/Models/ApproveReject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtoVen_MVC_UI.Models
+{
+    public class ApproveReject
+    {
+        public int id { get; set; }
+
+        public int ApprovalStatus { get; set; }
+
+    }
+}
